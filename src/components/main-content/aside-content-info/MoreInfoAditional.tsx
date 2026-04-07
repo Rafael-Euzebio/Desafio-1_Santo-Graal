b1be86ccd33b007693bde2f925e350d652c8cb75
claudio-inacio <inacio.unipar@gmail.com>
2026-04-07 08:59:22 -0300

    feat: create main struct

--- src/components/main-content/aside-content-info/MoreInfoAditional.tsx
+++ src/components/main-content/aside-content-info/MoreInfoAditional.tsx
@@ -1,31 +1,11 @@
+import { lastInfosAside, nextSeasonAside } from "../../../data/asideContentInfo"
+import { AditionalInfoCard } from "./card/AditionalInfoCard"
+
 export const MoreInfoAditional = () => {
     return (
-        <main className="rounded-tl-2xl rounded-tr-2xl border border-zinc-700/60  bg-center p-6  shadow-lg">
-            <h1 className="mb-4 border-b border-zinc-400/30 pb-3 text-4xl font-bold">
-                O Jogo do Abate
-            </h1>
-
-            <div className="space-y-5 text-lg leading-8 ">
-                <p>
-                    A atual temporada de Jujutsu Kaisen, <strong>“O Jogo do Abate”</strong>,
-                    mostra os feiticeiros presos em um ritual mortal criado para gerar caos,
-                    evolução forçada e confrontos de alto nível entre usuários de energia
-                    amaldiçoada.
-                </p>
-
-                <p>
-                    Nesse arco, os participantes precisam sobreviver dentro de colônias,
-                    acumular pontos e enfrentar inimigos extremamente perigosos. Ao mesmo
-                    tempo, os protagonistas tentam impedir os planos de Kenjaku e evitar
-                    que o número de mortes continue aumentando.
-                </p>
-
-                <p>
-                    A carnificina se espalha por Tóquio enquanto os participantes enfrentam
-                    maldições e feiticeiros poderosos em uma batalha sem misericórdia. Esse
-                    é um dos momentos mais tensos, estratégicos e brutais de toda a obra.
-                </p>
-            </div>
-        </main>
+        <div className="grid grid-cols-1 gap-4 xl:grid-cols-2">
+            <AditionalInfoCard title="Últimas Informações" aditionalInfocontent={lastInfosAside} />
+            <AditionalInfoCard title="Próxima temporada" aditionalInfocontent={nextSeasonAside} />
+        </div>
     )
 }
--- src/components/main-content/aside-content-info/card/CardTitle.tsx
+++ src/components/main-content/aside-content-info/card/CardTitle.tsx
@@ -5,7 +5,6 @@ interface CardTitleProps {
 export const CardTitle = ({ title }: CardTitleProps) => {
     return (
 
-
         <div className="mb-4 flex items-center gap-2 border-b border-zinc-300 pb-3">
             <span className="text-sm font-semibold text-zinc-600">
                 {title}
