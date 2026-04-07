d2e5e799ba4ffa49fb6e36419237f51a20b962bb
claudio-inacio <inacio.unipar@gmail.com>
2026-04-07 10:37:44 -0300

    feat: implement grid to layout

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,23 @@ import { characters } from "./data/characters";
 
 export default function App() {
   return (
-    <div>
+    <div className="min-h-screen">
       <Header />
-      <NavListPersons characters={characters} selectedCharacterId={1} onSelectCharacter={() => { }} />
-      <MainContent />
-      <CharactersDetailComponent />
+      <div className="pt-16">
+        <div className="grid gap-4 p-4 
+      grid-cols-1
+      md:grid-cols-1
+      lg:grid-cols-[260px_minmax(0,1fr)_320px]"
+        >
+          <NavListPersons
+            characters={characters}
+            selectedCharacterId={1}
+            onSelectCharacter={() => { }}
+          />
+          <MainContent />
+          <CharactersDetailComponent />
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/header/HeaderComponent.tsx
+++ src/components/header/HeaderComponent.tsx
@@ -5,7 +5,7 @@ import MenuButton from "./MenuButton"
 
 export default function Header() {
   return (
-    <header className="flex items-center md:justify-center justify-around px-4 h-16 bg-black">
+    <header className="fixed top-0 left-0 z-50 flex h-16 w-full items-center justify-between border-b border-zinc-800 bg-black px-4">
       <MenuButton />
       <ImageLogo logo={logo1} />
     </header>
